01e5a32e53a3191edda15eab657a4f5119be4ef9
Elvin Thudugala <elvin@outlook.co.nz>
2019-04-17 15:28:59 +1200

    nugetspec update

--- NuGet/Copylibs.ps1
+++ NuGet/Copylibs.ps1
@@ -1,3 +1,10 @@
+Remove-Item ".\Temp\*" -Recurse
+
 Copy-Item "..\scr\Plugin.LocalNotification\bin\Release\Plugin.LocalNotification.*.nupkg" -Destination '.\Temp\nupkg.zip'
-Expand-Archive -Path '.\Temp\nupkg.zip' -DestinationPath Temp
-Copy-Item '.\Temp\lib' -Destination '.\' -Recurse -ErrorAction SilentlyContinue
+#Copy-Item "..\scr\Plugin.LocalNotification\bin\Release\Plugin.LocalNotification.*.snupkg" -Destination '.\Temp\snupkg.zip'
+
+Expand-Archive -Path '.\Temp\nupkg.zip' -DestinationPath "Temp\pkg"
+#Expand-Archive -Path '.\Temp\snupkg.zip' -DestinationPath "Temp\spkg"
+
+Copy-Item '.\Temp\pkg\lib\*' -Destination '.\pkglib' -Recurse -ErrorAction SilentlyContinue
+#Copy-Item '.\Temp\spkg\lib\*' -Destination '.\spkglib' -Recurse -ErrorAction SilentlyContinue
