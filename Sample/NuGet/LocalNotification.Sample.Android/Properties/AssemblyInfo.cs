a9ecb9e127e0dd9dac0de060d06dd00b2a587b37
Elvin <elvin@outlook.co.nz>
2019-09-13 09:17:13 +1200

    sample update to 4.0.6

--- Sample/NuGet/LocalNotification.Sample.Android/Properties/AssemblyInfo.cs
+++ Sample/NuGet/LocalNotification.Sample.Android/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 #if DEBUG
 [assembly: Application(Debuggable = true)]
 #else
