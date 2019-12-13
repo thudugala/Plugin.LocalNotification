61725ec6fe18aa1d9736eb4b5a19164155630ffc
Elvin <elvint@nhc.maori.nz>
2019-12-13 15:58:40 +1300

    GitHubPackages

---
+++ GitHubPackages/SourceAdd.cmd
@@ -0,0 +1,1 @@
+nuget source Add -Name "GitHub" -Source "https://nuget.pkg.github.com/thudugala/index.json" -UserName thudugala -Password GH_TOKEN
---
+++ GitHubPackages/publish.cmd
@@ -0,0 +1,3 @@
+nuget push "Plugin.LocalNotification.4.1.0.nupkg" -Source "GitHub"
+
+nuget push "Plugin.LocalNotification.4.1.0.snupkg" -Source "GitHub"
