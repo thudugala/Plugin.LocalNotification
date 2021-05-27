b19e8690afcaaf13cbf887a6c143f1b59e0b26c5
Elvin <elvint@nhc.maori.nz>
2021-05-28 09:16:25 +1200

    Sample for v5

---
+++ Sample/NuGet v520/LocalNotification.Sample/NotificationPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalNotification.Sample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotificationPage : ContentPage
+    {
+        public NotificationPage(int tabCount)
+        {
+            InitializeComponent();
+
+            TapCountLabel.Text = $"Tap Count {tabCount}";
+        }
+    }
+}
