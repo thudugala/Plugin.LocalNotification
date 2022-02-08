76dffd63df7fe870a1a7f213396d3096c4869f99
Elvin Thudugala <elvin.thudugala@datacom.co.nz>
2022-02-08 17:40:03 +1300

    added WakeLock Permission for Android

--- Source/Plugin.LocalNotification/Platform/Droid/ManifestInfo.cs
+++ Source/Plugin.LocalNotification/Platform/Droid/ManifestInfo.cs
@@ -3,6 +3,7 @@
 using Android;
 using Android.App;
 
+[assembly: UsesPermission(Manifest.Permission.WakeLock)]
 [assembly: UsesPermission(Manifest.Permission.ReceiveBootCompleted)]
 //[assembly: UsesPermission(Manifest.Permission.ScheduleExactAlarm)]
 [assembly: UsesPermission("android.permission.SCHEDULE_EXACT_ALARM")]
