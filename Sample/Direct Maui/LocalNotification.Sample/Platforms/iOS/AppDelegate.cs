59e13e15abf0aedaef3916e4713c404b2bd03517
Elvin Thudugala <elvin.thudugala@datacom.co.nz>
2022-05-18 18:00:21 +1200

    Added iOS statup code

--- Sample/Direct Maui/LocalNotification.Sample/Platforms/iOS/AppDelegate.cs
+++ Sample/Direct Maui/LocalNotification.Sample/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,5 @@
 ﻿using Foundation;
+using UIKit;
 
 namespace LocalNotification.Sample;
 
@@ -6,4 +7,22 @@ namespace LocalNotification.Sample;
 public class AppDelegate : MauiUIApplicationDelegate
 {
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        // only call this method if you want to handel push notifications
+        // Plugin.LocalNotification.NotificationCenter.SetCustomUserNotificationCenterDelegate(new CustomUserNotificationCenterDelegate());
+
+        // Ask the user for permission to show notifications on iOS 10.0+ at startup.
+        // If not asked at startup, user will be asked when showing the first notification.
+        Plugin.LocalNotification.NotificationCenter.AskPermission();
+
+        return base.FinishedLaunching(application, launchOptions);
+    }
+
+    public override async void WillEnterForeground(UIApplication application)
+    {
+        await Plugin.LocalNotification.NotificationCenter.ResetApplicationIconBadgeNumber(application);
+        base.WillEnterForeground(application);
+    }
 }
