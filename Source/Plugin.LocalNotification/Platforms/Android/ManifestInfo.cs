762321ef33d5127013464eb7ddbcbaa28c7619ef
Elvin Thudugla <elvin@outlook.co.nz>
2023-01-14 11:39:22 +1300

    add android POST_NOTIFICATIONS

--- Source/Plugin.LocalNotification/Platforms/Android/ManifestInfo.cs
+++ Source/Plugin.LocalNotification/Platforms/Android/ManifestInfo.cs
@@ -2,6 +2,9 @@
 
 using Android;
 using Android.App;
+#if ANDROID
+using Microsoft.Maui.Controls.PlatformConfiguration;
+#endif
 
 [assembly: UsesPermission(Manifest.Permission.WakeLock)]
 [assembly: UsesPermission(Manifest.Permission.ReceiveBootCompleted)]
@@ -11,8 +14,16 @@ using Android.App;
 #if ANDROID
 [assembly: UsesPermission(Manifest.Permission.ScheduleExactAlarm)]
 [assembly: System.Reflection.AssemblyMetadata("IsTrimmable", "True")]
+
+#if NET7_0_OR_GREATER
+[assembly: UsesPermission(Manifest.Permission.PostNotifications)]
+#else
+[assembly: UsesPermission("android.permission.POST_NOTIFICATIONS")]
+#endif
+
 #elif MONOANDROID
 [assembly: UsesPermission("android.permission.SCHEDULE_EXACT_ALARM")]
+[assembly: UsesPermission("android.permission.POST_NOTIFICATIONS")]
 [assembly: LinkerSafe]
 #endif
 
