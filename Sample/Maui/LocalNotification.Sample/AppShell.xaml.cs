6d7913e32442e6a095c68f344aca630564dc3285
Elvin Thudugala <elvin@outlook.co.nz>
2026-07-12 12:12:27 +1200

    direct refrence example

---
+++ Sample/Maui/LocalNotification.Sample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalNotification.Sample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
